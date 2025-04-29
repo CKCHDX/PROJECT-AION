9eed4aa65038a8763b5193eb68cacaf796f326de
CKCHDX <153451620+CKCHDX@users.noreply.github.com>
2025-04-29 22:17:47 +0200

    Delete AION directory

--- AION/FAIL/kernel/memory.h
+++
@@ -1,9 +0,0 @@
-#ifndef MEMORY_H
-#define MEMORY_H
-
-#include <stddef.h>
-
-void init_memory(void);
-void* kmalloc(size_t size);
-
-#endif
--- AION/FAIL/kernel/vision.h
+++
@@ -1,7 +0,0 @@
-#ifndef VISION_H
-#define VISION_H
-
-void capture_frame(void);
-void detect_motion(void);
-
-#endif
