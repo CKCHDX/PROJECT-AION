a4f71a826e5dd7fff24a12f870b03766636d1896
CKCHDX <153451620+CKCHDX@users.noreply.github.com>
2025-05-13 11:37:00 +0200

    Delete AION directory

--- AION/kernel/aion/aion_vga.h
+++
@@ -1,9 +0,0 @@
-#ifndef AION_VGA_H
-#define AION_VGA_H
-
-void vga_clear_screen(char color);
-void vga_printf(const char* fmt, ...);
-void vga_putchar(char c);
-void emergency_shutdown(int reason);
-
-#endif
--- AION/kernel/aion/io/effector.h
+++
@@ -1,7 +0,0 @@
-// kernel/aion/io/effector.h
-#ifndef EFFECTOR_H
-#define EFFECTOR_H
-
-void execute_action(float action);
-
-#endif
